97259883188a2051de210ce0d0a051719c0af96a
Metin Demir <metmirr@gmail.com>
2023-05-27 17:52:00 +0300

    feat: add Account and Chain entities and mapping handler for them

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,5 +1,34 @@
-import { SpotLimitOrder, Transaction } from '../types'
+import { Account, Chain, Contract, SpotLimitOrder, Transaction } from '../types'
 import { CosmosMessage, CosmosTransaction } from '@subql/types-cosmos'
+
+async function createAccountsIfNotExists(
+  payer: string,
+  granter: string,
+  height: number,
+  timestamp: bigint,
+): Promise<void> {
+  let payerAcc = await Account.get(payer)
+  let granterAcc = await Account.get(granter)
+
+  if (!payerAcc && payer !== '') {
+    payerAcc = Account.create({
+      id: payer,
+      blockHeight: height,
+      timestamp,
+      address: payer,
+    })
+    await payerAcc.save()
+  }
+  if (!granterAcc && granter !== '') {
+    granterAcc = Account.create({
+      id: granter,
+      blockHeight: height,
+      timestamp,
+      address: granter,
+    })
+    await granterAcc.save()
+  }
+}
 
 export async function handleTransaction(_tx: CosmosTransaction): Promise<void> {
   const { fee } = _tx.decodedTx.authInfo
@@ -8,11 +37,12 @@ export async function handleTransaction(_tx: CosmosTransaction): Promise<void> {
     idx,
     block: {
       block: {
-        header: { height, time },
+        header: { height, time, chainId },
       },
     },
     tx: { gasUsed, code },
   } = _tx
+
   const transactionRecord = Transaction.create({
     id: `${hash}-${idx}`,
     blockHeight: height,
@@ -22,6 +52,64 @@ export async function handleTransaction(_tx: CosmosTransaction): Promise<void> {
     status: code === 0 ? 'success' : 'failed',
   })
   await transactionRecord.save()
+
+  if (fee) {
+    const { payer, granter } = fee
+    await createAccountsIfNotExists(payer, granter, height, BigInt(time.valueOf()))
+  }
+  let chain = await Chain.get(chainId)
+  if (chain) {
+    chain.blockHeight = height
+    chain.timestamp = BigInt(time.valueOf())
+    chain.gasUsed = chain.gasUsed + BigInt(gasUsed)
+    chain.txCount = chain.txCount + 1
+    chain.failedTxCount = chain.failedTxCount + (Boolean(code) ? 1 : 0)
+  } else {
+    chain = Chain.create({
+      id: chainId,
+      chainId,
+      blockHeight: height,
+      timestamp: BigInt(time.valueOf()),
+      gasUsed: BigInt(gasUsed),
+      txCount: 1,
+      failedTxCount: Boolean(code) ? 1 : 0,
+    })
+  }
+  await chain.save()
+}
+
+type ExecuteContractCompatMessage = {
+  sender: string
+  contract: string
+  msg: any
+  funds: string
+}
+
+export async function handleContractExecute(msg_: CosmosMessage<ExecuteContractCompatMessage>): Promise<void> {
+  const { msg, tx, block } = msg_
+  const { decodedMsg } = msg
+
+  const isFailed = Boolean(tx.tx.code) // code = 0 is success
+  let contract = await Contract.get(decodedMsg.contract)
+
+  if (contract) {
+    contract.txCount = contract.txCount + 1
+    contract.failedTxCount = contract.failedTxCount + (isFailed ? 1 : 0)
+    contract.timestamp = BigInt(block.header.time.valueOf())
+    contract.blockHeight = block.header.height
+  } else {
+    const { contract: address } = decodedMsg
+    contract = Contract.create({
+      id: address,
+      address,
+      blockHeight: block.header.height,
+      timestamp: BigInt(block.header.time.valueOf()),
+      txCount: 1,
+      failedTxCount: isFailed ? 1 : 0,
+      gasUsed: BigInt(tx.tx.gasUsed),
+    })
+  }
+  await contract.save()
 }
 
 type SpotLimitOrderMessage = {
@@ -38,6 +126,7 @@ type SpotLimitOrderMessage = {
   }
 }
 
+// MsgCreateDerivativeLimitOrder
 export async function handleMessage(msg: CosmosMessage<SpotLimitOrderMessage>): Promise<void> {
   //logger.info(JSON.stringify(msg));
   const spotLimitOrder = SpotLimitOrder.create({
@@ -54,15 +143,4 @@ export async function handleMessage(msg: CosmosMessage<SpotLimitOrderMessage>): 
     amount: BigInt(msg.msg.decodedMsg.order.orderInfo.price) * BigInt(msg.msg.decodedMsg.order.orderInfo.quantity),
   })
   // await spotLimitOrder.save();
-}
-
-type ExecuteContractCompatMessage = {
-  sender: string
-  contract: string
-  msg: any
-  funds: string
-}
-
-export async function handleContractExecute(msg: CosmosMessage<ExecuteContractCompatMessage>): Promise<void> {
-  // logger.info(JSON.stringify(msg.msg.decodedMsg))
 }
